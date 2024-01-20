244fc897ccf65e85b7199199d795a46560ba36e2
Yash Kotalwar <yashkotalwar10@gmail.com>
2024-01-21 00:31:34 +0530

    removed folder

--- FE/FE-NoteWise/src/app/entity/meetingInfo.ts
+++
@@ -1,8 +0,0 @@
-export interface MeetingInfo{
-    meetingId: number;
-    meetingTitle: string;
-    meetingNoteStatus: boolean;
-    meetingNotes: File;
-    mailSendStatus: boolean;
-    attendiesEmails: string[];
-}
